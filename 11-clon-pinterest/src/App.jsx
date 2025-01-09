7cf880763ee1d229f947457f23920d7391cd3dcf
sinname <nexxuz2017@gmail.com>
2025-01-09 11:52:29 -0500

    first commit

---
+++ 11-clon-pinterest/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Header } from "./components/Header";
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <h2>container</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ 11-clon-pinterest/src/components/Header.jsx
@@ -0,0 +1,26 @@
+export function Header() {
+  return (
+    <header>
+      <ul>
+        <li>
+          <a href="">Logo</a>
+        </li>
+        <li>
+          <a href="">Inicio</a>
+        </li>
+        <li>
+          <a href="">Hoy</a>
+        </li>
+        <li>
+          <a href="">Crear</a>
+        </li>
+        <li>
+          <input type="search" />
+        </li>
+        <li>
+          <a href="">User</a>
+        </li>
+      </ul>
+    </header>
+  );
+}
